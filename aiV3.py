984d3b7589ac45afdafa07a057fc1bc29d0c2137
John <john.orr10@outlook.com>
2025-10-07 16:10:52 +0100

    adding testing files to directory

--- aiV3.py
+++ aiV3.py
@@ -2,30 +2,46 @@ import random as r
 import numpy as np
 import getDataNP as getData
 
+
+class myAI:
+    def __init__(self, structure) -> None:
+        self.layer_counts = []
+        self.list_of_connections = []
+        self.list_of_biases = []
+        for num_nodes in structure:
+            self.create_random_layer(num_nodes)
+
+    def predict(self, input_data):
+        pass
+
+    def train(self, input_data, correct_output):
+        pass
+
+    def test_accuracy(self, test_data, answers):
+        pass
+  
+    def create_random_layer(self, num_new_nodes):
+        pass
+
+
 number_of_input_node = 784 
-struture = [50, 30, 10] # last layer is the class layer (has to be 10)
+output_nodes = 10
+struture = [number_of_input_node, 50, 30, output_nodes]
 learning_rate = 0.01
 
 
 # training on all the data 
 digit_images_file = "files/train-images-idx3-ubyte.gz"
 digit_labels_file = "files/train-labels-idx1-ubyte.gz"
-im = getData.get_data(300, digit_images_file, digit_images_file)
+lables, images = getData.get_data(300, digit_images_file, digit_images_file)
 
 
-class myAI:
-    lables, inputs = getData.get_data(300)
-    def __init__(self, structure) -> None:
-        pass
-  
-    def initialize_network(self, structure):
-        self.num_nodes = 
-        self.layer_counts = []
-        self.list_of_connections = []
-        self.list_of_biases = []
-        for num_nodes in structure:
-        self.create_random_layer(num_nodes)
-
-    def create_random_layer(self, num_new_nodes):
+my_ai = myAI(struture)
+my_ai.train(images, lables)
 
 
+# testing the accuracy
+digit_images_file = "files/t10k-images-idx3-ubyte"
+digit_labels_file = "files/t10k-labels-idx1-ubyte"
+test_lables, test_images = getData.get_data(100, digit_images_file, digit_labels_file)
+my_ai.test_accuracy(test_images, test_lables)
--- testing.py
+++ testing.py
@@ -3,34 +3,54 @@ from getDataNP import get_data
 
 class TestGetData(unittest.TestCase):
     def setUp(self):
-        self.labels_path = "files/train-labels-idx1-ubyte.gz"
-        self.images_path = "files/train-images-idx3-ubyte.gz"
+        self.train_labels_path = "files/train-labels.idx1-ubyte.gz"
+        self.train_images_path = "files/train-images.idx3-ubyte.gz"
+        self.test_labels_path = "files/t10k-labels.idx1-ubyte.gz"
+        self.test_images_path = "files/t10k-images.idx3-ubyte.gz"
 
-    def test_get_data_valid_count(self):
+    def test_get_data_valid_count_training_files(self):
         count = 10
-        labels, images = get_data(count, self.labels_path, self.images_path)
+        labels, images = get_data(count, self.train_labels_path, self.train_images_path)
+        self.assertEqual(labels.shape, (count,))
+        self.assertEqual(images.shape[0], count)
+        self.assertEqual(images.shape[1:], (28, 28))  # Assuming MNIST image size
+    
+    def test_get_data_valid_count_test_files(self):
+        count = 10
+        labels, images = get_data(count, self.test_labels_path, self.test_images_path)
         self.assertEqual(labels.shape, (count,))
         self.assertEqual(images.shape[0], count)
         self.assertEqual(images.shape[1:], (28, 28))  # Assuming MNIST image size
 
     def test_get_data_zero_count(self):
         count = 0
-        labels, images = get_data(count, self.labels_path, self.images_path)
+        labels, images = get_data(count, self.train_labels_path, self.train_images_path)
         self.assertEqual(labels.shape, (0,))
         self.assertEqual(images.shape, (0, 28, 28))
 
     def test_get_data_exceeding_count(self):
         count = 70000  # Exceeds MNIST training set size
-        labels, images = get_data(count, self.labels_path, self.images_path)
+        labels, images = get_data(count, self.train_labels_path, self.train_images_path)
         self.assertEqual(labels.shape, (60000,))
         self.assertEqual(images.shape[0], 60000)
         self.assertEqual(images.shape[1:], (28, 28))
 
     def test_get_data_negative_count(self):
         count = -5
-        labels, images = get_data(count, self.labels_path, self.images_path)
+        labels, images = get_data(count, self.train_labels_path, self.train_images_path)
         self.assertEqual(labels.shape, (0,))
         self.assertEqual(images.shape, (0, 28, 28))
+
+class TestGetDataFiles(unittest.TestCase):
+    def setUp(self):
+        self.train_labels_path = "files/train-labels-idx1-ubyte.gz"
+        self.train_images_path = "files/train-images-idx3-ubyte.gz"
+
+    def test_training_images(self):
+        labels, images = get_data(1, self.train_labels_path, self.train_images_path)
+        print(images)
+        self.assertTrue(True)
+
 
 class TestAi(unittest.TestCase):
     def test_placeholder(self):
